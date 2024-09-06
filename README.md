cd2fdb59e9b22907746bf7f62c32d2557ef90a97
weifw <weifengwei@xiongmaocar.com>
2024-09-06 15:42:03 +0800

    feat:最最基础的springboot框架

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # yijiahe
+
+# 项目介绍
+1. 项目名称：yijiahe
+2. 项目技术简介: springboot mybatiys-plus vue3 element-plus mysql redis
---
+++ admin/src/main/java/com/yijiahe/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.yijiahe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author weifw
+ * @date 2024/9/6
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
